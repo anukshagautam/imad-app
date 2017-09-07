936ffd463f0f6398437c6786ea8e3ed30870eefd
anukshagautam <31386790+anukshagautam@users.noreply.github.com>
2017-09-07 08:21:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,13 @@
 console.log('Loaded!');
 
 //change the text of main text div
-var element=document.getElementById=("main text");
+var element=document.getElementById=('main-text');
 
 element.innerHTML = 'abcd';
+
+//move image
+var img = document.getElementById = ('madi');
+
+img.onclick= function(){
+    img.style.marginLeft='100px';
+}
