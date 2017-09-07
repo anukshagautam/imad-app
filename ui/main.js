980fbbbfa2d6464aa769ed9e966b2b66008cc253
anukshagautam <31386790+anukshagautam@users.noreply.github.com>
2017-09-07 08:31:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ element.innerHTML = 'abcd';
 //move image
 var img = document.getElementById('madi');
 
-madi.onclick = function (){ 
+img.onclick = function (){ 
     img.style.marginLeft='100px';
 };
